49d600b2588f480178228e5594bec2b6361dfd27
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-01-05 01:45:39 -0200

    Fixing setup.py.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name="listutils",
       version="1.7.0",
-      description="A vim-like in python made from scratch.",
+      description="Django template tag to split list into sub lists.",
       packages=["listutils"],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
@@ -12,6 +12,7 @@ setup(name="listutils",
       download_url='https://github.com/iogf/listutils/releases',
       keywords=['django', 'listutils', 'split', 'lists'],
       classifiers=[])
+
 
 
 
