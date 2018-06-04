c702e0bf4cef0a3ed3d462140a2d7011def2fd85
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2018-06-04 11:13:27 -0300

    Removing unecessary code.

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from distutils.core import setup
 setup(name="django-listutils",
       version="1.0.0",
       description="Django template tag to split list into sub lists.",
-      packages=["listutils"],
+      packages=["listutils", 'listutils.templatetags'],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
       url='https://github.com/iogf/listutils',
       download_url='https://github.com/iogf/listutils/releases',
       keywords=['django', 'listutils', 'split', 'lists'],
       classifiers=[])
+
 
 
 
